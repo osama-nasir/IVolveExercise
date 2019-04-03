bcdbe0355ae85d275b624f4a3f535e5e6bcf2f51
osama-nasir <37299330+osama-nasir@users.noreply.github.com>
2019-04-03 17:50:22 +0500

    Update script.sh

--- CustomContainer/script.sh
+++ CustomContainer/script.sh
@@ -2,3 +2,5 @@
 echo Creating Image
 cd nexus-blobstore-google-cloud
 docker build -t nexus-osama:0.1 .
+docker tag nexus-osama:0.1 gcr.io/nexuskubernetes/nexus-osama:0.1
+docker push gcr.io/nexuskubernetes/nexus-osama:0.1
