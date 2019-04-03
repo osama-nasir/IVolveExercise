6cb703bfad9e0b4c4fe558c99f6017dc8de0a1b1
osama-nasir <37299330+osama-nasir@users.noreply.github.com>
2019-04-03 18:27:09 +0500

    Update script.sh

--- KubernetesDeploy/script.sh
+++ KubernetesDeploy/script.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 echo Kubernetes Deploy
 gcloud config set compute/zone asia-south1-a
-gcloud container clusters create nexusKuber
 gcloud container clusters create nexus-kuber
 gcloud container clusters get-credentials nexus-kuber
-kubectl run nexus-kubernetes --image=nexus-osama:0.1 --port 8080
+kubectl run nexus-kubernetes --image=gcr.io/nexuskubernetes/nexus-osama:0.1	 --port 8080
 kubectl expose deployment nexus-kubernetes --type="LoadBalancer"
 kubectl get service nexus-kubernetes
 
