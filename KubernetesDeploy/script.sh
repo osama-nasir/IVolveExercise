31edb7e4a09f46829e7bd9c2c2f2ccf985994309
osama-nasir <37299330+osama-nasir@users.noreply.github.com>
2019-04-03 17:12:53 +0500

    Update script.sh

--- KubernetesDeploy/script.sh
+++ KubernetesDeploy/script.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-echo Creating Image
-cd nexus-blobstore-google-cloud
-docker build -t nexus-osama:0.1 .
+echo Kubernetes Deploy
+gcloud config set compute/zone asia-south1-a
+gcloud container clusters create nexusKuber
+gcloud container clusters create nexus-kuber
+gcloud container clusters get-credentials nexus-kuber
+kubectl run nexus-kubernetes --image=nexus-osama:0.1 --port 8080
+kubectl expose deployment nexus-kubernetes --type="LoadBalancer"
+kubectl get service nexus-kubernetes
+
